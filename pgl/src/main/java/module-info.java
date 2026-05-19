a327f8b60d510a8896394298020958bd43d22330
Yotacaz <mmjcrissot@laposte.net>
2026-05-19 23:23:57 +0200

    removed some warning and use case uml diagram

--- pgl/src/main/java/module-info.java
+++ pgl/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.cy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens fr.cy to javafx.fxml;
     opens fr.cy.controller to javafx.fxml;
