4c94242d7ed2f5eabb7d5ec2a90873f137a633ea
adlnkg <adlene.koroghli@gmail.com>
2026-05-21 19:27:32 +0200

    graph class update

---
+++ pgl/src/main/java/fr/cy/model/graph/IdManager.java
@@ -0,0 +1,50 @@
+package fr.cy.model.graph;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Gère la génération et la réutilisation d'identifiants entiers.
+ * <p>
+ * Les identifiants libérés via {@link #releaseId(int)} sont réattribués en
+ * priorité
+ * avant de créer de nouveaux identifiants.
+ */
+public class IdManager {
+    private int nextId;
+
+    private Queue<Integer> freeIds;
+
+    /**
+     * Crée un gestionnaire d'identifiants initialisé à 0.
+     */
+    public IdManager() {
+        this.nextId = 0;
+        this.freeIds = new ArrayDeque<>();
+    }
+
+    /**
+     * Génère un identifiant disponible.
+     * <p>
+     * Si un identifiant a été libéré, il est réutilisé. Sinon, un nouvel
+     * identifiant incrémental est créé.
+     *
+     * @return un identifiant disponible
+     */
+    public int generateId() {
+
+        if (!freeIds.isEmpty()) {
+            return freeIds.poll();
+        }
+        return nextId++;
+    }
+
+    /**
+     * Libère un identifiant pour permettre sa réutilisation ultérieure.
+     *
+     * @param id identifiant à remettre dans la file des identifiants libres
+     */
+    public void releaseId(int id) {
+        freeIds.add(id);
+    }
+}
