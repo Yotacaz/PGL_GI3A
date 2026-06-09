7f23868a68dd03209a27b81f734eb353f5e14def
Duvoye Alois <alois.duvoye@gmail.com>
2026-06-09 11:29:15 +0200

    prépartion pour reverse edge

--- pgl/src/main/java/fr/cy/model/graph/GraphConfig.java
+++ pgl/src/main/java/fr/cy/model/graph/GraphConfig.java
@@ -7,7 +7,7 @@ public class GraphConfig implements Serializable {
     private static final long serialVersionUID = 1L;
     public static final double DEFAULT_NODE_CAPACITY = 20.0;
     public static final double DEFAULT_EDGE_LENGTH = 10.0;
-    public static final double DEFAULT_EDGE_WIDTH = 5.0;
+    public static final double DEFAULT_EDGE_WIDTH = 2.0;
     public static final boolean DEFAULT_EDGE_DIRECTED = false;
     // public static final double DEFAULT_EDGE_MAX_AGENT_SPEED = AgentSettings.getInstance().getRUNNING_SPEED()*1.5; // Default max speed for agents on edges
 }
